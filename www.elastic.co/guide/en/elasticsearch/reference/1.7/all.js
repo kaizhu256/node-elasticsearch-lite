ef112b609f2d3f760aab262fef5cdad82866c21e
kai zhu <kaizhu256@gmail.com>
2017-06-19 23:44:25 +0800

    images

--- www.elastic.co/guide/en/elasticsearch/reference/1.7/all.js
+++ www.elastic.co/guide/en/elasticsearch/reference/1.7/all.js
@@ -17,12 +17,10 @@
 ,$s/<!--.*-->//gc
 
 
-,$s/<script\>/<noscript/gc
-,$s/;/;\r/gc
-,$s/#guide/.guide/gc
 ,$s/#guide\>/.guide/gc
-,$s/#content/.content/gc
+,$s/#content\>/.content/gc
 ,$s/id="content"/class="content"/gc
+,$s/id="guide"/class="guide"/gc
 ,$s/href="\([a-bA-Z0-9_\-]*\).html"/href="#\1"/gc
 ,$s/href="\([\w\-]*\).html"/href="#\1"/gc
 ,$s/\/body >/\/body>/gc
