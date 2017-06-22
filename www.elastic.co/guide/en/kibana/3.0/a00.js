871d3ae3bd206079b5ad5031b398a60b64ed5dee
kai zhu <kaizhu256@gmail.com>
2017-06-22 23:43:54 +0800

    a00.js

---
+++ www.elastic.co/guide/en/kibana/3.0/a00.js
@@ -0,0 +1,540 @@
+cat index.html > all.html
+cat kibana-guide.html >> all.html
+cat using-kibana-for-the-first-time.html >> all.html
+cat import-some-data.html >> all.html
+cat accessing-the-kibana-interface.html >> all.html
+cat the-first-search.html >> all.html
+cat configuring-another-index.html >> all.html
+cat _next_steps.html >> all.html
+cat working-with-queries-and-filters.html >> all.html
+cat our-dashboard.html >> all.html
+cat queries.html >> all.html
+cat multiple-queries.html >> all.html
+cat colors-and-legends.html >> all.html
+cat filters.html >> all.html
+cat _more_reading.html >> all.html
+cat rows-and-panels.html >> all.html
+cat loading-a-blank-dashboard.html >> all.html
+cat adding-a-row.html >> all.html
+cat the-row-controls.html >> all.html
+cat adding-panels.html >> all.html
+cat collapsing-and-expanding-rows.html >> all.html
+cat editing-rows.html >> all.html
+cat moving-and-removing-panels.html >> all.html
+cat moving-and-removing-rows.html >> all.html
+cat _next_steps_2.html >> all.html
+cat saving-and-loading-dashboards.html >> all.html
+cat saving-your-awesome-dashboard.html >> all.html
+cat recalling-your-dashboard.html >> all.html
+cat sharing-dashboards.html >> all.html
+cat saving-a-static-dashboard.html >> all.html
+cat _next_steps_3.html >> all.html
+cat _dashboard_schema.html >> all.html
+cat services.html >> all.html
+cat pulldown.html >> all.html
+cat nav.html >> all.html
+cat loader.html >> all.html
+cat rows-array.html >> all.html
+cat index-settings.html >> all.html
+cat the-rest.html >> all.html
+cat importing-schemas.html >> all.html
+cat templated-and-scripted-dashboards.html >> all.html
+cat the-dashboard-directory.html >> all.html
+cat templated-dashboards-.json.html >> all.html
+cat scripted-dashboards-.js.html >> all.html
+cat _parameters.html >> all.html
+cat panels.html >> all.html
+cat _bettermap.html >> all.html
+cat _parameters_2.html >> all.html
+cat _column.html >> all.html
+cat _parameters_3.html >> all.html
+cat _goal.html >> all.html
+cat _parameters_4.html >> all.html
+cat _histogram.html >> all.html
+cat _parameters_5.html >> all.html
+cat _hits.html >> all.html
+cat _parameters_6.html >> all.html
+cat _map.html >> all.html
+cat _parameters_7.html >> all.html
+cat _sparklines.html >> all.html
+cat _parameters_8.html >> all.html
+cat _table.html >> all.html
+cat _parameters_9.html >> all.html
+cat _terms.html >> all.html
+cat _parameters_10.html >> all.html
+cat _text.html >> all.html
+cat _parameters_11.html >> all.html
+cat _trends.html >> all.html
+cat _parameters_12.html >> all.html
+
+
+
+[
+'#getting-started',
+'#getting-started',
+'#_basic_concepts',
+'#_installation',
+'#_exploring_your_cluster',
+'#_cluster_health',
+'#_list_all_indices',
+'#_create_an_index',
+'#_index_and_query_a_document',
+'#_delete_an_index',
+'#_modifying_your_data',
+'#_updating_documents',
+'#_deleting_documents',
+'#_batch_processing',
+'#_exploring_your_data',
+'#_the_search_api',
+'#_introducing_the_query_language',
+'#_executing_searches',
+'#_executing_filters',
+'#_executing_aggregations',
+'#_conclusion',
+'#setup',
+'#setup-configuration',
+'#setup-service',
+'#setup-service-win',
+'#setup-dir-layout',
+'#setup-repositories',
+'#setup-upgrade',
+'#breaking-changes',
+'#breaking-changes-1.6',
+'#breaking-changes-1.5',
+'#breaking-changes-1.4',
+'#breaking-changes-1.0',
+'#_system_and_settings',
+'#_stats_and_info_apis',
+'#_indices_apis_2',
+'#_index_request',
+'#_search_requests',
+'#_multi_fields',
+'#_stopwords',
+'#_dates_without_years',
+'#_parameters',
+'#_return_values',
+'#_deprecations',
+'#_percolator_2',
+'#api-conventions',
+'#multi-index',
+'#common-options',
+'#url-access-control',
+'#docs',
+'#docs-index_',
+'#docs-get',
+'#docs-delete',
+'#docs-update',
+'#docs-multi-get',
+'#docs-bulk',
+'#docs-delete-by-query',
+'#docs-bulk-udp',
+'#docs-termvectors',
+'#docs-multi-termvectors',
+'#search',
+'#search-search',
+'#search-uri-request',
+'#search-request-body',
+'#search-request-query',
+'#search-request-from-size',
+'#search-request-sort',
+'#search-request-source-filtering',
+'#search-request-fields',
+'#search-request-script-fields',
+'#search-request-fielddata-fields',
+'#search-request-post-filter',
+'#search-request-highlighting',
+'#search-request-rescore',
+'#search-request-search-type',
+'#search-request-scroll',
+'#search-request-preference',
+'#search-request-explain',
+'#search-request-version',
+'#search-request-index-boost',
+'#search-request-min-score',
+'#search-request-named-queries-and-filters',
+'#search-request-inner-hits',
+'#search-template',
+'#search-shards',
+'#search-aggregations',
+'#search-aggregations-metrics-min-aggregation',
+'#search-aggregations-metrics-max-aggregation',
+'#search-aggregations-metrics-sum-aggregation',
+'#search-aggregations-metrics-avg-aggregation',
+'#search-aggregations-metrics-stats-aggregation',
+'#search-aggregations-metrics-extendedstats-aggregation',
+'#search-aggregations-metrics-valuecount-aggregation',
+'#search-aggregations-metrics-percentile-aggregation',
+'#search-aggregations-metrics-percentile-rank-aggregation',
+'#search-aggregations-metrics-cardinality-aggregation',
+'#search-aggregations-metrics-geobounds-aggregation',
+'#search-aggregations-metrics-top-hits-aggregation',
+'#search-aggregations-metrics-scripted-metric-aggregation',
+'#search-aggregations-bucket-global-aggregation',
+'#search-aggregations-bucket-filter-aggregation',
+'#search-aggregations-bucket-filters-aggregation',
+'#search-aggregations-bucket-missing-aggregation',
+'#search-aggregations-bucket-nested-aggregation',
+'#search-aggregations-bucket-reverse-nested-aggregation',
+'#search-aggregations-bucket-children-aggregation',
+'#search-aggregations-bucket-terms-aggregation',
+'#search-aggregations-bucket-significantterms-aggregation',
+'#search-aggregations-bucket-range-aggregation',
+'#search-aggregations-bucket-daterange-aggregation',
+'#search-aggregations-bucket-iprange-aggregation',
+'#search-aggregations-bucket-histogram-aggregation',
+'#search-aggregations-bucket-datehistogram-aggregation',
+'#search-aggregations-bucket-geodistance-aggregation',
+'#search-aggregations-bucket-geohashgrid-aggregation',
+'#search-facets',
+'#search-facets-terms-facet',
+'#search-facets-range-facet',
+'#search-facets-histogram-facet',
+'#search-facets-date-histogram-facet',
+'#search-facets-filter-facet',
+'#search-facets-query-facet',
+'#search-facets-statistical-facet',
+'#search-facets-terms-stats-facet',
+'#search-facets-geo-distance-facet',
+'#search-facets-migrating-to-aggs',
+'#search-suggesters',
+'#search-suggesters-term',
+'#search-suggesters-phrase',
+'#search-suggesters-completion',
+'#suggester-context',
+'#search-multi-search',
+'#search-count',
+'#search-exists',
+'#search-validate',
+'#search-explain',
+'#search-percolate',
+'#search-more-like-this',
+'#search-field-stats',
+'#indices',
+'#indices-create-index',
+'#indices-delete-index',
+'#indices-get-index',
+'#indices-exists',
+'#indices-open-close',
+'#indices-put-mapping',
+'#indices-get-mapping',
+'#indices-get-field-mapping',
+'#indices-types-exists',
+'#indices-delete-mapping',
+'#indices-aliases',
+'#indices-update-settings',
+'#indices-get-settings',
+'#indices-analyze',
+'#indices-templates',
+'#indices-warmers',
+'#indices-status',
+'#indices-stats',
+'#indices-segments',
+'#indices-recovery',
+'#indices-clearcache',
+'#indices-flush',
+'#indices-synced-flush',
+'#indices-refresh',
+'#indices-optimize',
+'#indices-shadow-replicas',
+'#_node_level_settings_related_to_shadow_replicas',
+'#indices-upgrade',
+'#cat',
+'#cat-alias',
+'#cat-allocation',
+'#cat-count',
+'#cat-fielddata',
+'#cat-health',
+'#cat-indices',
+'#cat-master',
+'#cat-nodes',
+'#cat-pending-tasks',
+'#cat-plugins',
+'#cat-recovery',
+'#cat-thread-pool',
+'#cat-shards',
+'#index-pattern',
+'#relocation',
+'#states',
+'#cat-segments',
+'#cluster',
+'#cluster-health',
+'#cluster-state',
+'#cluster-stats',
+'#cluster-pending',
+'#cluster-reroute',
+'#cluster-update-settings',
+'#cluster-nodes-stats',
+'#cluster-nodes-info',
+'#cluster-nodes-hot-threads',
+'#cluster-nodes-shutdown',
+'#query-dsl',
+'#query-dsl-queries',
+'#query-dsl-match-query',
+'#query-dsl-multi-match-query',
+'#query-dsl-bool-query',
+'#query-dsl-boosting-query',
+'#query-dsl-common-terms-query',
+'#query-dsl-constant-score-query',
+'#query-dsl-dis-max-query',
+'#query-dsl-filtered-query',
+'#query-dsl-flt-query',
+'#query-dsl-flt-field-query',
+'#query-dsl-function-score-query',
+'#query-dsl-fuzzy-query',
+'#query-dsl-geo-shape-query',
+'#query-dsl-has-child-query',
+'#query-dsl-has-parent-query',
+'#query-dsl-ids-query',
+'#query-dsl-indices-query',
+'#query-dsl-match-all-query',
+'#query-dsl-mlt-query',
+'#query-dsl-nested-query',
+'#query-dsl-prefix-query',
+'#query-dsl-query-string-query',
+'#query-dsl-simple-query-string-query',
+'#query-dsl-range-query',
+'#query-dsl-regexp-query',
+'#query-dsl-span-first-query',
+'#query-dsl-span-multi-term-query',
+'#query-dsl-span-near-query',
+'#query-dsl-span-not-query',
+'#query-dsl-span-or-query',
+'#query-dsl-span-term-query',
+'#query-dsl-term-query',
+'#query-dsl-terms-query',
+'#query-dsl-top-children-query',
+'#query-dsl-wildcard-query',
+'#query-dsl-minimum-should-match',
+'#query-dsl-multi-term-rewrite',
+'#query-dsl-template-query',
+'#query-dsl-filters',
+'#query-dsl-and-filter',
+'#query-dsl-bool-filter',
+'#query-dsl-exists-filter',
+'#query-dsl-geo-bounding-box-filter',
+'#query-dsl-geo-distance-filter',
+'#query-dsl-geo-distance-range-filter',
+'#query-dsl-geo-polygon-filter',
+'#query-dsl-geo-shape-filter',
+'#query-dsl-geohash-cell-filter',
+'#query-dsl-has-child-filter',
+'#query-dsl-has-parent-filter',
+'#query-dsl-ids-filter',
+'#query-dsl-indices-filter',
+'#query-dsl-limit-filter',
+'#query-dsl-match-all-filter',
+'#query-dsl-missing-filter',
+'#query-dsl-nested-filter',
+'#query-dsl-not-filter',
+'#query-dsl-or-filter',
+'#query-dsl-prefix-filter',
+'#query-dsl-query-filter',
+'#query-dsl-range-filter',
+'#query-dsl-regexp-filter',
+'#query-dsl-script-filter',
+'#query-dsl-term-filter',
+'#query-dsl-terms-filter',
+'#query-dsl-type-filter',
+'#mapping',
+'#mapping-fields',
+'#mapping-uid-field',
+'#mapping-id-field',
+'#mapping-type-field',
+'#mapping-source-field',
+'#mapping-all-field',
+'#mapping-analyzer-field',
+'#mapping-boost-field',
+'#mapping-parent-field',
+'#mapping-field-names-field',
+'#mapping-routing-field',
+'#mapping-index-field',
+'#mapping-size-field',
+'#mapping-timestamp-field',
+'#mapping-ttl-field',
+'#mapping-types',
+'#mapping-core-types',
+'#mapping-array-type',
+'#mapping-object-type',
+'#mapping-root-object-type',
+'#mapping-nested-type',
+'#mapping-ip-type',
+'#mapping-geo-point-type',
+'#mapping-geo-shape-type',
+'#mapping-attachment-type',
+'#mapping-date-format',
+'#mapping-dynamic-mapping',
+'#mapping-conf-mappings',
+'#mapping-meta',
+'#mapping-transform',
+'#_get_transformed',
+'#_immutable_transformation',
+'#analysis',
+'#analysis-analyzers',
+'#analysis-standard-analyzer',
+'#analysis-simple-analyzer',
+'#analysis-whitespace-analyzer',
+'#analysis-stop-analyzer',
+'#analysis-keyword-analyzer',
+'#analysis-pattern-analyzer',
+'#analysis-lang-analyzer',
+'#analysis-snowball-analyzer',
+'#analysis-custom-analyzer',
+'#analysis-tokenizers',
+'#analysis-standard-tokenizer',
+'#analysis-edgengram-tokenizer',
+'#analysis-keyword-tokenizer',
+'#analysis-letter-tokenizer',
+'#analysis-lowercase-tokenizer',
+'#analysis-ngram-tokenizer',
+'#analysis-whitespace-tokenizer',
+'#analysis-pattern-tokenizer',
+'#analysis-uaxurlemail-tokenizer',
+'#analysis-pathhierarchy-tokenizer',
+'#analysis-classic-tokenizer',
+'#analysis-thai-tokenizer',
+'#analysis-tokenfilters',
+'#analysis-standard-tokenfilter',
+'#analysis-asciifolding-tokenfilter',
+'#analysis-length-tokenfilter',
+'#analysis-lowercase-tokenfilter',
+'#analysis-uppercase-tokenfilter',
+'#analysis-ngram-tokenfilter',
+'#analysis-edgengram-tokenfilter',
+'#analysis-porterstem-tokenfilter',
+'#analysis-shingle-tokenfilter',
+'#analysis-stop-tokenfilter',
+'#analysis-word-delimiter-tokenfilter',
+'#analysis-stemmer-tokenfilter',
+'#analysis-stemmer-override-tokenfilter',
+'#analysis-keyword-marker-tokenfilter',
+'#analysis-keyword-repeat-tokenfilter',
+'#analysis-kstem-tokenfilter',
+'#analysis-snowball-tokenfilter',
+'#analysis-phonetic-tokenfilter',
+'#analysis-synonym-tokenfilter',
+'#analysis-compound-word-tokenfilter',
+'#analysis-reverse-tokenfilter',
+'#analysis-elision-tokenfilter',
+'#analysis-truncate-tokenfilter',
+'#analysis-unique-tokenfilter',
+'#analysis-pattern-capture-tokenfilter',
+'#analysis-pattern_replace-tokenfilter',
+'#analysis-trim-tokenfilter',
+'#analysis-limit-token-count-tokenfilter',
+'#analysis-hunspell-tokenfilter',
+'#analysis-common-grams-tokenfilter',
+'#analysis-normalization-tokenfilter',
+'#analysis-cjk-width-tokenfilter',
+'#analysis-cjk-bigram-tokenfilter',
+'#analysis-delimited-payload-tokenfilter',
+'#analysis-keep-words-tokenfilter',
+'#analysis-keep-types-tokenfilter',
+'#analysis-classic-tokenfilter',
+'#analysis-apostrophe-tokenfilter',
+'#analysis-charfilters',
+'#analysis-mapping-charfilter',
+'#analysis-htmlstrip-charfilter',
+'#analysis-pattern-replace-charfilter',
+'#analysis-icu-plugin',
+'#modules',
+'#modules-cluster',
+'#modules-discovery',
+'#modules-discovery-azure',
+'#modules-discovery-ec2',
+'#modules-discovery-gce',
+'#modules-discovery-zen',
+'#modules-gateway',
+'#modules-gateway-local',
+'#modules-http',
+'#modules-indices',
+'#modules-memcached',
+'#modules-network',
+'#modules-node',
+'#modules-tribe',
+'#modules-plugins',
+'#modules-scripting',
+'#modules-advanced-scripting',
+'#modules-threadpool',
+'#modules-thrift',
+'#modules-transport',
+'#modules-snapshots',
+'#index-modules',
+'#index-modules-analysis',
+'#index-modules-allocation',
+'#delayed-allocation',
+'#recovery-prioritization',
+'#_total_shards_per_node',
+'#disk',
+'#index-modules-slowlog',
+'#index-modules-merge',
+'#index-modules-store',
+'#index-modules-mapper',
+'#index-modules-translog',
+'#index-modules-cache',
+'#index-modules-shard-query-cache',
+'#index-modules-fielddata',
+'#fielddata-formats',
+'#index-modules-similarity',
+'#testing',
+'#testing-framework',
+'#why-randomized-testing',
+'#using-elasticsearch-test-classes',
+'#unit-tests',
+'#integration-tests',
+'#parametrized-tests',
+'#randomized-testing',
+'#assertions',
+'#glossary',
+].forEach(function (query) {
+    try {
+        console.assert(document.querySelector(query));
+    } catch (errorCaught) {
+        console.error(errorCaught);
+    }
+});
+
+
+
+,$s/<\/body>\(.*\n\)\{-\}.*<body>/\r/gc
+,$s/\n\n\n*/\r\r/gc
+[
+  '.col-xs-12.col-sm-4.col-md-4',
+].forEach(function(query) {
+    Array.from(document.querySelectorAll(query)).forEach(function (element) {
+        element.remove();
+    });
+});
+
+[
+  'body > *',
+].forEach(function(query) {
+    Array.from(document.querySelectorAll(query)).forEach(function (element) {
+        if (!element.classList.contains('main-container')) {
+            element.remove();
+        }
+    });
+});
+
+    Array.from(document.body.childNodes).forEach(function (element) {
+        if (!element.classList.contains('main-container')) {
+            element.remove();
+        }
+    });
+
+
+[
+  'section#content',
+].forEach(function(query) {
+    Array.from(document.querySelectorAll(query)).forEach(function (element) {
+        Array.from(element.childNodes).forEach(function (element) {
+            if (!(element.classList && element.classList.contains('content-wrapper'))) {
+                element.remove();
+            }
+        });
+    });
+});
+
+
+
+,$s/<!-- <div class="pageheader">\(.*\n\)\{6\}.*-->/\r/gc
+
