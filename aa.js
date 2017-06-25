bc09bfe9c5dc992edf25df7357894880611346e1
kai zhu <kaizhu256@gmail.com>
2017-06-26 04:01:24 +0800

    script

--- aa.js
+++ aa.js
@@ -1,16 +1,28 @@
-/\n\n\n\n*/\r\r/gc
+,$s/\n\n\n\n*/\r\r/gc
 
-/<\/body *>\(.*\n\)\{-\}.*<body *>//gc
-483 substitutions on 483 lines
+,$s/<\/body *>\(.*\n\)\{-\}.*<body *>//gc
+483
 
-/<script\>/<script2/gc
-/\<script>/script2>/gc
+,$s/<script\>/<script2/gc
+,$s/\<script>/script2>/gc
 7751
 
+,$s/<script2\>\(.*\n\)\{-\}.*<\/script2>//gc
+7751
+
+,$s/<iframe\>\(.*\n\)\{-\}.*<\/iframe>//gc
+484
+
+,$s/<noscript\>/<noscript3/gc
+,$s/\<noscript>/noscript3>/gc
+968
+
 [
-  'script2'
+  'script2',
+  'noscript'
 ].forEach(function (query) {
     Array.from(document.querySelectorAll(query)).forEach(function (element) {
         element.remove();
     })
 });
+document.documentElement.outerHTML
